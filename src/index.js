95f2d8d20d57c92dc843dbf57a390e1e8b149d49
Francesco Camillini <francesco.camillini@gmail.com>
2020-01-10 13:03:50 +0100

    feat: auto install dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require('fs-extra');
+const npm = require('npm');
 const installRecipe = require('./utils/installRecipe');
 const recipes = require('./recipes');
 const packageJsonPath = `${process.cwd()}/package.json`;
@@ -12,6 +13,9 @@ try {
         installRecipe(packageJson)(recipe)
     );
     fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson));
+    npm.load(() => {
+        npm.commands.install([], () => {});
+    });
 } catch (e) {
     console.log(e);
     process.exit(1);
